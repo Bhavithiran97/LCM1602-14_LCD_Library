31f259f2013676577de1b65bd818efb89a2641ba
Bhavithiran <bhavithiran97@gmail.com>
2021-02-03 13:26:52 +0800

    Initial commit

--- Examples/hello_world_i2c.py
+++ Examples/hello_world_i2c.py
@@ -28,7 +28,7 @@
 
 from LCD_I2C import *    #import LCD_I2C library
 
-lcd = LCD(sda=2, scl=3)  # Create LCD object with LCD's sda pin connected to PICO's sda pin 2, LCD's sck pin connectoed to Pico's scl pin 3
+lcd = LCD(sda=2, scl=3)  # Create LCD object with LCD's sda pin connected to PICO's sda pin 2, LCD's sck pin connected to Pico's scl pin 3
 lcd.set_cursor(0,0)          # Set the cursor at first column, first row
 lcd.write("Hello World")     # Write string to the LCD   
 utime.sleep(1)               # Delay for 1 second
--- Examples/hello_world_spi.py
+++ Examples/hello_world_spi.py
@@ -28,7 +28,7 @@
 
 from LCD_SPI import *    #import LCD_SPI library
 
-lcd = LCD(sck=2, scl=3)  # Create LCD object with LCD's sck pin connected to PICO's sck pin 2, LCD's sda pin connectoed to Pico's tx pin 3
+lcd = LCD(sck=2, scl=3)  # Create LCD object with LCD's sck pin connected to PICO's sck pin 2, LCD's sda pin connected to Pico's tx pin 3
 lcd.set_cursor(0,0)          # Set the cursor at first column, first row
 lcd.write("Hello World")     # Write string to the LCD   
 utime.sleep(1)               # Delay for 1 second
